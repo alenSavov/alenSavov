143daa7caa231dfb8ce949f8ca6f69a3c0cfe58c
alenSavov <playmatte@gmail.com>
2019-04-02 14:25:32 +0300

    ProgressBarIncrease.js

--- scripts/main/ProgressBarIncrease.js
+++ scripts/main/ProgressBarIncrease.js
@@ -13,31 +13,31 @@ function InitializeBars(direction) {
 
         if(direction === "down"){
             $("#pb1").animate({
-                width: "60%"
+                width: "80%"
             }, 999);
             $("#pb2").animate({
-                width: "40%"
+                width: "70%"
             }, 999);
             $("#pb3").animate({
                 width: "80%"
             }, 999);
             $("#pb4").animate({
-                width: "70%"
+                width: "60%"
             }, 999);
             $("#pb5").animate({
-                width: "60%"
+                width: "50%"
             }, 999);
             $("#pb6").animate({
-                width: "70%"
+                width: "50%"
             }, 999);
             $("#pb7").animate({
-                width: "70%"
+                width: "50%"
             }, 999);
             $("#pb8").animate({
-                width: "60%"
+                width: "65%"
             }, 999);
             $("#pb9").animate({
-                width: "50%"
+                width: "45%"
             }, 999);
         }
     });
